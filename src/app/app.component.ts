30be25d8a8e4dc18e1e139b31872d0a1a4aa5ca3
PdskDev <prestadesk@gmail.com>
2022-05-11 01:46:15 +0200

    Product list page, table, paginator...done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,64 @@
 import { DialogComponent } from './dialog/dialog.component';
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ApiService } from './services/api.service';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular13Crud';
 
-  constructor(private dialog: MatDialog) {}
+  displayedColumns: string[] = [
+    'date',
+    'name',
+    'category',
+    'condition',
+    'price',
+    'comment',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dialog: MatDialog, private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
 
   openDialog() {
     this.dialog.open(DialogComponent, {
       width: '35%',
     });
+  }
+
+  getAllProducts() {
+    this.api.getProduct().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,6 +1,7 @@
 import { ApiService } from './../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -11,7 +12,11 @@ export class DialogComponent implements OnInit {
   ProductConditionListing = ['Nouveau', 'Occasion', 'Reconditionné'];
   productForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private api: ApiService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private api: ApiService,
+    private dialogRef: MatDialogRef<DialogComponent>
+  ) {}
 
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
@@ -30,6 +35,8 @@ export class DialogComponent implements OnInit {
       this.api.postProduct(this.productForm.value).subscribe({
         next: (res) => {
           alert('Le produit a été ajouté avec succès');
+          this.productForm.reset();
+          this.dialogRef.close('Enregistrer');
         },
         error: (err) => {
           alert("Une erreur est survenue. L'ajout du produit a échoué");
